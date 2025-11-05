f9d7fb3b7d85216f5a9e0b39812cb0a16d60a9cf
Paul Osborne <paul.osborne@fastly.com>
2025-11-05 11:46:15 -0600

    Target stable asm! and set MSRV
    On non-nightly compilers, the precision crate code continued
    to fallback on the C version; Inline assembly has been stable
    for some time now, so target it and set the MSRV to 1.59.

--- build.rs
+++ build.rs
@@ -1,16 +1,12 @@
 #[cfg(not(any(target_arch = "wasm32", target_arch = "wasm64")))]
-use rustc_version::{version_meta, Channel};
-
-#[cfg(not(any(target_arch = "wasm32", target_arch = "wasm64")))]
 fn asm_detect() {
-    let using_nightly = version_meta().unwrap().channel == Channel::Nightly;
     let asm_capable_target = cfg!(any(
         target_arch = "x86",
         target_arch = "x86_64",
         target_arch = "aarch64",
         target_arch = "riscv64",
     ));
-    if using_nightly && asm_capable_target {
+    if asm_capable_target {
         println!("cargo:rustc-cfg=asm");
     } else {
         cc::Build::new()
